0fa7228566284e8e508f957f33574e37a5086b05
Ash Zaki <ashleya.zaki@gmail.com>
2019-06-27 21:26:13 -0400

    greeting changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); //to get access to express library
 const app = express(); //create appliacation that represent express app
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'boo'});
 });
 
 const PORT = process.env.PORT || 5000;
