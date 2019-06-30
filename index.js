4495647ac11d5180324a23578883f12977f1ad2c
Ash Zaki <ashleya.zaki@gmail.com>
2019-06-30 15:59:07 -0400

    adding passport.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express'); //to get access to express library
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express(); //create appliacation that represent express app
 
-app.get('/', (req, res) => {
-    res.send({bye: 'boo'});
-});
+//Test**
+// app.get('/', (req, res) => {
+//     res.send({bye: 'boo'});
+// });
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT);  
