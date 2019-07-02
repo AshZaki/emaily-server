49a47e82ab2cfb096c5fafd8ccba751f7ac7688c
Ash Zaki <ashleya.zaki@gmail.com>
2019-07-02 15:29:45 -0400

    connected mongoose to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express'); //to get access to express library
+const mongoose = require('mongoose');
+const keys =require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true});
 
 const app = express(); //to get access to express library
 //create appliacation that represent express app
