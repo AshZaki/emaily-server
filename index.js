1d4454216a4d79184c0aed65ddb64d5b001f8596
Ash Zaki <ashleya.zaki@gmail.com>
2019-07-02 16:58:52 -0400

    fist test on saving model instances

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express'); //to get access to express library
 const mongoose = require('mongoose');
 const keys =require('./config/keys');
-require('./services/passport');
 require('./models/User')
+require('./services/passport');
+
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true});
 
-const app = express(); //to get access to express library
-//create appliacation that represent express app
+const app = express(); //create appliacation that represent express app
 
 require('./routes/authRoutes')(app);
 
